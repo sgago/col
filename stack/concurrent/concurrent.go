9d3a9e88792b2812421cbb99aba3ee2bb80b4119
Steven Gago <steven.a.gago@gmail.com>
2022-06-24 14:56:38 -0500

    concstack - added a concurrent stack data struct, tests

---
+++ stack/concurrent/concurrent.go
@@ -0,0 +1,114 @@
+// Package concurrent provides an unbounded, slice-backed,
+// thread-safe stack data structure.
+// A stack data structure provides two main operations:
+//
+//	1. Push which adds an element to the top of the stack.
+//	2. Pop which removes and returns the top element of the stack.
+package concurrent
+
+import (
+	"sync"
+
+	"github.com/sgago/collections/slice"
+)
+
+// An unbounded, slice-backed, thread-safe stack data structure with type T elements.
+type concstack[T any] struct {
+	elements []T
+	mu       sync.Mutex
+}
+
+// Allocates and initializes a new stack with type T elements.
+//
+// Values are pushed by index in ascending (non-decreasing) order.
+// In other wrods, the first value, values[0], will be pushed first.
+// The last value, values[len(values)-1] will be pushed last and appear
+// on top of the stack.
+func New[T any](capacity int, values ...T) *concstack[T] {
+	s := concstack[T]{elements: make([]T, 0, capacity)}
+
+	s.PushMany(values...)
+
+	return &s
+}
+
+// Push adds a value to the top of the stack.
+func (s *concstack[T]) Push(value T) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.elements = append(s.elements, value)
+}
+
+// PushMany adds multiple values on top of the stack.
+//
+// Values are pushed into the stack by index in ascending (non-decreasing) order.
+// In other words, the first value, values[0], will be pushed first.
+// The last value, values[len(values)-1] will be pushed last and appear on top of the stack.
+//
+// If no values are supplied, then nothing will be pushed.
+func (s *concstack[T]) PushMany(values ...T) {
+	if len(values) != 0 {
+		s.mu.Lock()
+		defer s.mu.Unlock()
+
+		s.elements = append(s.elements, values...)
+	}
+}
+
+// Pop removes and returns the top element of the stack.
+//
+// This method panics if the stack is empty.
+func (s *concstack[T]) Pop() T {
+	if s.elements == nil || len(s.elements) == 0 {
+		panic("The stack is empty.")
+	}
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	last := slice.Last(s.elements)
+
+	s.elements = slice.RemoveLast(s.elements)
+
+	return last
+}
+
+// Pop returns the top element of the stack.
+//
+// This method panics if the stack is empty.
+func (s *concstack[T]) Peek() T {
+	if s.elements == nil || len(s.elements) == 0 {
+		panic("The stack is empty.")
+	}
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	return s.elements[len(s.elements)-1]
+}
+
+// Count returns the number of elements in the stack.
+func (s *concstack[T]) Count() int {
+	return len(s.elements)
+}
+
+// Capacity returns the capacity of the stack.
+func (s *concstack[T]) Capacity() int {
+	return cap(s.elements)
+}
+
+// IsEmpty returns true if the stack has no elements;
+// otherwise, false.
+func (s *concstack[T]) IsEmpty() bool {
+	return s.Count() == 0
+}
+
+// Clear removes all elements from the stack.
+// It maintains the stack's existing capacity.
+func (s *concstack[T]) Clear() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.elements = slice.Clear(s.elements)
+}
---
+++ stack/concurrent/concurrent_test.go
@@ -0,0 +1,171 @@
+package concurrent
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew_WithNoValues_CountIsZero(t *testing.T) {
+	cap := 3
+
+	stack := New[int](cap)
+
+	assert.Zero(t, stack.Count())
+}
+
+func TestNew_WithNoValues_CapacityIsCorrect(t *testing.T) {
+	cap := 3
+
+	stack := New[int](cap)
+
+	assert.Equal(t, cap, stack.Capacity())
+}
+
+func TestNew_WithValues_ElementsAreCorrect(t *testing.T) {
+	cap := 5
+
+	values := make([]int, 0, cap)
+
+	for i := 0; i < cap; i++ {
+		values = append(values, i)
+	}
+
+	stack := New(cap, values...)
+
+	for i := 0; i < len(values); i++ {
+		assert.Equal(t, stack.elements[i], values[i])
+	}
+}
+
+func TestPush_WithValues_ElementsAreCorrect(t *testing.T) {
+	cap := 5
+
+	stack := New[int](cap)
+
+	values := make([]int, 0, cap)
+
+	for i := 0; i < cap; i++ {
+		stack.Push(i)
+	}
+
+	for i := 0; i < len(values); i++ {
+		assert.Equal(t, stack.elements[i], values[i])
+	}
+}
+
+func TestPushRange_WithNoValues_NothingPushed(t *testing.T) {
+	cap := 5
+
+	stack := New[int](cap)
+
+	for i := 0; i < cap; i++ {
+		stack.PushMany()
+	}
+
+	assert.Empty(t, stack.elements)
+}
+
+func TestPop_WithValues_PoppedValueIsCorrect(t *testing.T) {
+	cap := 5
+
+	values := make([]int, 0, cap)
+
+	for i := 0; i < cap; i++ {
+		values = append(values, i)
+	}
+
+	stack := New(cap, values...)
+
+	actual := stack.Pop()
+
+	assert.Equal(t, values[len(values)-1], actual)
+}
+
+func TestPop_WithValues_ElementsAreCorrect(t *testing.T) {
+	cap := 5
+
+	values := make([]int, 0, cap)
+
+	for i := 0; i < cap; i++ {
+		values = append(values, i)
+	}
+
+	stack := New(cap, values...)
+
+	stack.Pop()
+
+	for i := 0; i < len(values)-1; i++ {
+		assert.Equal(t, stack.elements[i], values[i])
+	}
+}
+
+func TestPeek_WithValues_PeekedValueIsCorrect(t *testing.T) {
+	cap := 5
+
+	values := make([]int, 0, cap)
+
+	for i := 0; i < cap; i++ {
+		values = append(values, i)
+	}
+
+	stack := New(cap, values...)
+
+	actual := stack.Peek()
+
+	assert.Equal(t, values[len(values)-1], actual)
+}
+
+func TestPeek_WithValues_ElementsAreCorrect(t *testing.T) {
+	cap := 5
+
+	values := make([]int, 0, cap)
+
+	for i := 0; i < cap; i++ {
+		values = append(values, i)
+	}
+
+	stack := New(cap, values...)
+
+	stack.Peek()
+
+	for i := 0; i < len(values); i++ {
+		assert.Equal(t, stack.elements[i], values[i])
+	}
+}
+
+func TestIsEmpty_WithElements_IsFalse(t *testing.T) {
+	cap := 1
+
+	stack := New(cap, 1)
+
+	assert.False(t, stack.IsEmpty())
+}
+
+func TestIsEmpty_WithNoElements_IsTrue(t *testing.T) {
+	cap := 1
+
+	stack := New[int](cap)
+
+	assert.True(t, stack.IsEmpty())
+}
+
+func TestClear_WithElements_CountIsZero(t *testing.T) {
+	cap := 3
+
+	stack := New(cap, 1, 2, 3)
+
+	stack.Clear()
+
+	assert.Equal(t, 0, stack.Count())
+}
+
+func TestClear_WithElements_CapacityIsSame(t *testing.T) {
+	cap := 3
+
+	stack := New(cap, 1, 2, 3)
+
+	stack.Clear()
+
+	assert.Equal(t, cap, stack.Capacity())
+}
