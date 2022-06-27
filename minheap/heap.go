656fe38fbbc2d11a44b3d89abb01c431551cb845
Steven Gago <steven.a.gago@gmail.com>
2022-06-27 12:25:37 -0500

    heap - fixed bugs, added tests

---
+++ minheap/heap.go
@@ -0,0 +1,176 @@
+package heap
+
+import (
+	"github.com/sgago/collections"
+	"github.com/sgago/collections/slice"
+)
+
+// The heap sort order, that is, a min or max heap.
+type HeapSort bool
+
+const (
+	// Indicates a minheap data structure.
+	Min HeapSort = true
+
+	// Indicates a maxheap data structure.
+	Max HeapSort = false
+)
+
+type heap[T any] struct {
+	elems      []collections.KeyValue[T]
+	bubbleUp   func(index int)
+	bubbleDown func(index int)
+}
+
+func New[T any](sort HeapSort, cap int, vals ...collections.KeyValue[T]) *heap[T] {
+	h := heap[T]{
+		elems: make([]collections.KeyValue[T], 0, cap),
+	}
+
+	if sort == Min {
+		h.bubbleUp = h.bubbleUpMinHeap
+		h.bubbleDown = h.bubbleDownMinHeap
+	} else {
+		h.bubbleUp = h.bubbleUpMaxHeap
+		h.bubbleDown = h.bubbleDownMaxHeap
+	}
+
+	for _, val := range vals {
+		h.Push(val.Key, val.Value)
+	}
+
+	return &h
+}
+
+func (h *heap[T]) Push(key int, val T) {
+	h.elems = append(
+		h.elems,
+		collections.KeyValue[T]{Key: key, Value: val})
+
+	h.bubbleUp(len(h.elems) - 1)
+}
+
+func (h *heap[T]) Pop() collections.KeyValue[T] {
+	if h.elems == nil || len(h.elems) == 0 {
+		panic("The minheap is empty.")
+	}
+
+	val := h.elems[0]
+
+	h.elems[0], _ = slice.Last(h.elems, nil)
+	h.elems = slice.RemoveLast(h.elems)
+
+	h.bubbleDown(0)
+
+	return val
+}
+
+func (h *heap[T]) Peek() collections.KeyValue[T] {
+	if h.elems == nil || len(h.elems) == 0 {
+		panic("The minheap is empty.")
+	}
+
+	return collections.KeyValue[T]{Key: h.elems[0].Key, Value: h.elems[0].Value}
+}
+
+// Count returns the number of elements in the heap.
+func (h *heap[T]) Count() int {
+	return len(h.elems)
+}
+
+// Capacity returns the capacity of the heap.
+func (h *heap[T]) Capacity() int {
+	return cap(h.elems)
+}
+
+// IsEmpty returns true if the heap has no elements;
+// otherwise, false.
+func (h *heap[T]) IsEmpty() bool {
+	return h.Count() == 0
+}
+
+// Clear removes all elements from the heap.
+// It maintains the heap's existing capacity.
+func (h *heap[T]) Clear() {
+	h.elems = slice.Clear(h.elems)
+}
+
+func (h *heap[T]) bubbleUpMinHeap(index int) {
+	parent := getParentIndex(index)
+
+	if h.elems[parent].Key > h.elems[index].Key {
+		h.elems = slice.Swap(h.elems, parent, index)
+
+		if parent > 0 {
+			h.bubbleUpMinHeap(parent)
+		}
+	}
+}
+
+func (h *heap[T]) bubbleUpMaxHeap(index int) {
+	parent := getParentIndex(index)
+
+	if h.elems[parent].Key < h.elems[index].Key {
+		h.elems = slice.Swap(h.elems, parent, index)
+
+		if parent > 0 {
+			h.bubbleUpMaxHeap(parent)
+		}
+	}
+}
+
+func (h *heap[T]) bubbleDownMinHeap(index int) {
+	if !h.isLeaf(index) {
+		left := getLeftChildIndex(index)
+		right := getRightChildIndex(index)
+
+		smallest := left
+		isRightEmpty := right >= len(h.elems)
+
+		if !isRightEmpty && h.elems[right].Key < h.elems[left].Key {
+			smallest = right
+		}
+
+		if h.elems[index].Key > h.elems[smallest].Key {
+			h.elems = slice.Swap(h.elems, smallest, index)
+
+			h.bubbleDownMinHeap(smallest)
+		}
+	}
+}
+
+func (maxheap *heap[T]) bubbleDownMaxHeap(index int) {
+	if !maxheap.isLeaf(index) {
+		left := getLeftChildIndex(index)
+		right := getRightChildIndex(index)
+
+		largest := left
+		isRightEmpty := right >= len(maxheap.elems)
+
+		if !isRightEmpty && maxheap.elems[left].Key > maxheap.elems[right].Key {
+			largest = left
+		}
+
+		if maxheap.elems[index].Key < maxheap.elems[largest].Key {
+			maxheap.elems = slice.Swap(maxheap.elems, largest, index)
+
+			maxheap.bubbleDownMaxHeap(largest)
+		}
+	}
+}
+
+func (h *heap[T]) isLeaf(index int) bool {
+	return len(h.elems) <= getLeftChildIndex(index)
+}
+
+func getParentIndex(index int) int {
+	return (index - 1) / 2
+}
+
+func getLeftChildIndex(index int) int {
+	return 2*index + 1
+}
+
+func getRightChildIndex(index int) int {
+	return 2*index + 2
+}
---
+++ minheap/heap_test.go
@@ -0,0 +1,153 @@
+package heap
+
+import (
+	"testing"
+
+	"github.com/sgago/collections"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPush_WithMinHeap_ElementsCorrect(t *testing.T) {
+	h := New[int](Min, 4)
+
+	h.Push(3, 3)
+	h.Push(2, 2)
+	h.Push(1, 1)
+	h.Push(4, 4)
+
+	assert.Equal(t, 4, len(h.elems))
+	assert.Equal(t, 1, h.elems[0].Key)
+	assert.Equal(t, 3, h.elems[1].Key)
+	assert.Equal(t, 2, h.elems[2].Key)
+	assert.Equal(t, 4, h.elems[3].Key)
+}
+
+func TestPush_WithMaxHeap_ElementsCorrect(t *testing.T) {
+	h := New[int](Max, 4)
+
+	h.Push(3, 3)
+	h.Push(2, 2)
+	h.Push(1, 1)
+	h.Push(4, 4)
+
+	assert.Equal(t, 4, len(h.elems))
+	assert.Equal(t, 4, h.elems[0].Key)
+	assert.Equal(t, 3, h.elems[1].Key)
+	assert.Equal(t, 1, h.elems[2].Key)
+	assert.Equal(t, 2, h.elems[3].Key)
+}
+
+func TestPop_WithMinHeap_ElementsCorrect(t *testing.T) {
+	h := New[int](Min, 4)
+
+	h.Push(3, 3)
+	h.Push(2, 2)
+	h.Push(1, 1)
+	h.Push(4, 4)
+
+	values := make([]int, 0, 4)
+
+	values = append(values, h.Pop().Key)
+	values = append(values, h.Pop().Key)
+	values = append(values, h.Pop().Key)
+	values = append(values, h.Pop().Key)
+
+	assert.Equal(t, 4, len(values))
+	assert.Equal(t, 1, values[0])
+	assert.Equal(t, 2, values[1])
+	assert.Equal(t, 3, values[2])
+	assert.Equal(t, 4, values[3])
+}
+
+func TestPop_WithMaxHeap_ElementsCorrect(t *testing.T) {
+	h := New[int](Max, 4)
+
+	h.Push(3, 3)
+	h.Push(2, 2)
+	h.Push(1, 1)
+	h.Push(4, 4)
+
+	values := make([]int, 0, 4)
+
+	values = append(values, h.Pop().Key)
+	values = append(values, h.Pop().Key)
+	values = append(values, h.Pop().Key)
+	values = append(values, h.Pop().Key)
+
+	assert.Equal(t, 4, len(values))
+	assert.Equal(t, 4, values[0])
+	assert.Equal(t, 3, values[1])
+	assert.Equal(t, 2, values[2])
+	assert.Equal(t, 1, values[3])
+}
+
+func TestPeek_WithMinHeap_IsCorrect(t *testing.T) {
+	h := New[int](Min, 4)
+
+	h.Push(3, 3)
+	h.Push(2, 2)
+	h.Push(1, 1)
+	h.Push(4, 4)
+
+	actual := h.Peek().Key
+
+	assert.Equal(t, 1, actual)
+}
+
+func TestPeek_WithMaxHeap_IsCorrect(t *testing.T) {
+	h := New[int](Max, 4)
+
+	h.Push(3, 3)
+	h.Push(2, 2)
+	h.Push(1, 1)
+	h.Push(4, 4)
+
+	actual := h.Peek().Key
+
+	assert.Equal(t, 4, actual)
+}
+
+func TestIsEmpty_WithElements_IsFalse(t *testing.T) {
+	cap := 1
+
+	h := New(Min, cap, collections.KeyValue[int]{
+		Key:   1,
+		Value: 1,
+	})
+
+	assert.False(t, h.IsEmpty())
+}
+
+func TestIsEmpty_WithNoElements_IsTrue(t *testing.T) {
+	cap := 1
+
+	h := New[int](Min, cap)
+
+	assert.True(t, h.IsEmpty())
+}
+
+func TestClear_WithElements_CountIsCorrect(t *testing.T) {
+	cap := 1
+
+	h := New(Min, cap, collections.KeyValue[int]{
+		Key:   1,
+		Value: 1,
+	})
+
+	h.Clear()
+
+	assert.True(t, h.IsEmpty())
+}
+
+func TestClear_WithElements_CapacityIsSame(t *testing.T) {
+	cap := 1
+
+	h := New(Min, cap, collections.KeyValue[int]{
+		Key:   1,
+		Value: 1,
+	})
+
+	h.Clear()
+
+	assert.Equal(t, cap, h.Capacity())
+}
